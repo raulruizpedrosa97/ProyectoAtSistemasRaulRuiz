6813cccc8d632bf6a05302dc6621e24690fb50e5
raulr <raulr@LAPTOP-JSHKNSET>
2022-07-05 18:46:40 +0200

    Primer commit

---
+++ proyecto_Raul_Ruiz_atSistemas/src/main/java/com/example/demo/ProyectoRaulRuizAtSistemasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRaulRuizAtSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRaulRuizAtSistemasApplication.class, args);
+	}
+
+}
